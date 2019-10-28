96f11d1774fef2e39d5f53b086bcf08e9d94b4c8
Kayla <kaynuhe@gmail.com>
2019-10-28 15:22:36 -0400

    feat: flickity images appear

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 (function() {
 
+    const $carousel = $('.project-carousel-container')
 
-    // Flickity
-    $('.project-carousel-container').flickity({
+    // $carousel.on('mouseenter.flickity', function() {
+    //     console.log('mouse has entered carousel')
+    // })
+
+
+
+    $carousel.flickity({
         // options
         cellAlign: 'left',
         contain: true
       });
+
+    //   $('.project-cell').on('mouseenter', (e) => {
+    //       console.log(e)
+    //       $(e).hide()
+    //       $('.project-description').show();
+    //   })
 
 
 })(jQuery);
